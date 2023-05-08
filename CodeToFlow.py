adbffd0d08814b9f37b4e5f7514255f51eb61be7
GordonChen19 <chengordon8@gmail.com>
2023-05-09 01:50:40 +0800

    cfg 1.0

--- CodeToFlow.py
+++ CodeToFlow.py
@@ -18,57 +18,57 @@ def code_to_cfg(code):
     for node in ast.walk(tree):
         if isinstance(node, ast.If):
             # If statement
-            cfg.add_edge(id(node.test), id(node.body[0]))
+            cfg.add_edge(node.test.lineno, node.body[0].lineno)
             if node.orelse:
-                cfg.add_edge(id(node.test), id(node.orelse[0]))
+                cfg.add_edge(node.test.lineno, node.orelse[0].lineno)
         elif isinstance(node, ast.While):
             # While loop
-            cfg.add_edge(id(node.test), id(node.body[0]))
-            cfg.add_edge(id(node.test), id(node))
-            cfg.add_edge(id(node), id(node.test))
+            cfg.add_edge(node.test.lineno, node.body[0].lineno)
+            cfg.add_edge(node.test.lineno, node.lineno)
+            cfg.add_edge(node.lineno, node.test.lineno)
         elif isinstance(node, ast.For):
             # For loop
-            cfg.add_edge(id(node.iter), id(node.target))
-            cfg.add_edge(id(node.iter), id(node.body[0]))
-            cfg.add_edge(id(node.body[-1]), id(node.iter))
+            cfg.add_edge(node.iter.lineno, node.target.lineno)
+            cfg.add_edge(node.iter.lineno, node.body[0].lineno)
+            cfg.add_edge(node.body[-1].lineno, node.iter.lineno)
         elif isinstance(node, ast.Try):
             # Try/except block
             for handler in node.handlers:
-                cfg.add_edge(id(handler), id(handler.body[0]))
+                cfg.add_edge(handler.lineno, handler.body[0].lineno)
             if node.orelse:
-                cfg.add_edge(id(node), id(node.orelse[0]))
+                cfg.add_edge(node.lineno, node.orelse[0].lineno)
         elif isinstance(node, ast.With):
             # With statement
-            cfg.add_edge(id(node.context_expr), id(node.body[0]))
+            cfg.add_edge(node.context_expr.lineno, node.body[0].lineno)
         elif isinstance(node, ast.FunctionDef):
             # Function definition
-            cfg.add_edge(id(node.args), id(node.body[0]))
+            print(f'function definition {node.lineno}')
+            cfg.add_edge(node.lineno, node.body[0].lineno)
             for n in node.body:
                 if isinstance(n, ast.Return):
-                    cfg.add_edge(id(n), id(node))
-        elif isinstance(node, ast.Return):
-            # Return statement
-            cfg.add_edge(id(node), id(node.value))
+                    cfg.add_edge(n.lineno, node.lineno)
+        # elif isinstance(node, ast.Return):
+        #     # Return statement
+        #     if node.value:
+        #         cfg.add_edge(node.lineno, node.value.lineno)
+        #     else:
+        #         cfg.add_edge(node.lineno, node.lineno)
         elif isinstance(node, ast.Break):
             # Break statement
-            cfg.add_node(id(node))
+            cfg.add_node(node.lineno)
         elif isinstance(node, ast.Continue):
             # Continue statement
-            cfg.add_node(id(node))
+            cfg.add_node(node.lineno)
         elif isinstance(node, ast.Expr):
             # Expression statement
-            cfg.add_edge(id(node), id(node.value))
+            cfg.add_edge(node.lineno, node.value.lineno)
         elif isinstance(node, ast.Assign):
             # Assignment statement
-            cfg.add_edge(id(node.value), id(node.targets[0]))
+            cfg.add_edge(node.value.lineno, node.targets[0].lineno)
+            
+    print(cfg.edges())
 
-    # Draw the CFG using matplotlib
-    pos = nx.drawing.nx_agraph.graphviz_layout(cfg, prog='dot')
-    nx.draw_networkx_nodes(cfg, pos)
-    nx.draw_networkx_edges(cfg, pos)
-    nx.draw_networkx_labels(cfg, pos)
-    plt.show()
-    
+
     
 code = """
 def foo(n):
