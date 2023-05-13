9f275a2cefc9010f1f7cab46ae674efce75f0681
GordonChen19 <chengordon8@gmail.com>
2023-05-13 15:45:22 +0800

    ide 1.0.1

--- Website/views.py
+++ Website/views.py
@@ -4,6 +4,8 @@ from .models import *
 import json
 import sqlite3 as sql
 
+import subprocess
+
 conn=sql.connect('database.db')
 c=conn.cursor()
 
@@ -12,3 +14,14 @@ views=Blueprint('views',__name__)
 @views.route('/',methods=['GET','POST'])
 def home():
     return render_template("home.html",user=current_user)
+
+@views.route('/all_projects', methods=['GET', 'POST'])
+def project():
+    code = request.form.get('code')
+    if code:
+        result = subprocess.run(code, stdout=subprocess.PIPE, shell=True)
+        output = result.stdout.decode('utf-8')
+        print(output)
+    else:
+        output = ''
+    return render_template("ide.html", user=current_user)
