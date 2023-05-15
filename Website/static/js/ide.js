84fa9e1978d12248991a8e292e2449b69d338673
GordonChen19 <chengordon8@gmail.com>
2023-05-15 11:48:50 +0800

    stash

--- Website/static/js/ide.js
+++ Website/static/js/ide.js
@@ -1,36 +1,81 @@
-let editor;
+const JAVA_KEY = "62";
+const CPP_KEY = "53";
+const PYTHON_KEY = "70";
+const BASE_URL = "http://34.72.83.62/submissions"
+function codeEditor(lang_id) {
+  var editor = ace.edit("editor");
+  editor.setTheme("ace/theme/twilight");
 
-window.onload = function() {
-    editor = ace.edit("editor");
-    editor.setTheme("ace/theme/monokai");
-    editor.session.setMode("ace/mode/c_cpp");
+
+  console.log("id" + lang_id )
+  $(document).ready(function () {
+    $("button").click(function () {
+      let code = editor.getValue();
+      $("#ans").html("Loading...");
+      console.log(code);
+      let data = {
+        source_code: code,
+        language_id: lang_id,
+        number_of_runs: "1",
+        stdin: "Judge0",
+        expected_output: null,
+        cpu_time_limit: "2",
+        cpu_extra_time: "0.5",
+        wall_time_limit: "5",
+        memory_limit: "128000",
+        stack_limit: "64000",
+        max_processes_and_or_threads: "60",
+        enable_per_process_and_thread_time_limit: false,
+        enable_per_process_and_thread_memory_limit: false,
+        max_file_size: "1024",
+      };
+      console.log(data)
+      let request = $.ajax({
+        url: BASE_URL,
+        type: "post",
+        data: data,
+      });
+
+      const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+      // Callback handler that will be called on success
+      request.done(async function (response, textStatus, jqXHR) {
+        // Log a message to the console
+        console.log("Hooray, it worked!");
+        let token = response.token;
+        await new Promise((resolve) => setTimeout(resolve, 5000)); // 5 sec
+        let second_request = $.ajax({
+          url: BASE_URL + "/"+ token,
+          type: "get",
+        });
+        second_request.done(function (response) {
+          console.log(response.stdout);
+          $("#ans").html(response.stdout);
+        });
+      });
+    });
+  });
+  if(lang_id==PYTHON_KEY)
+      editor.setValue("def execute(): \n\t for i in range(10):\n\t\t print i \nexecute()")
+  //java
+  if(lang_id==JAVA_KEY){
+
+      let javacode = `public class Main{
+  public static void main(String args[]){
+    System.out.println("hello");
+  }
 }
+`;
 
-function changeLanguage() {
+  editor.setValue(javacode)
 
-    let language = $("#languages").val();
+  }if(lang_id==CPP_KEY){
+      let cppcode = `#include <iostream>
+using namespace std;
+  int main() {
+      cout<<"Hello World"; \n
+}`
+      editor.setValue(cppcode)
+  }
 
-    if(language == 'c' || language == 'cpp')editor.session.setMode("ace/mode/c_cpp");
-    else if(language == 'php')editor.session.setMode("ace/mode/php");
-    else if(language == 'python')editor.session.setMode("ace/mode/python");
-    else if(language == 'node')editor.session.setMode("ace/mode/javascript");
-}
 
-function executeCode() {
-
-    $.ajax({
-
-        url: "Website/compiler.php",
-
-        method: "POST",
-
-        data: {
-            language: $("#languages").val(),
-            code: editor.getSession().getValue()
-        },
-
-        success: function(response) {
-            $(".output").text(response)
-        }
-    })
-}
+} 
