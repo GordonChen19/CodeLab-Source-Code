056875c2d2d3bde6384f692bb5ac31bc90a8c405
ivanyjq <iyuen001@ntu.edu.sg>
2023-05-20 01:19:16 +0800

    create room function added

--- Website/views.py
+++ Website/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, url_for, redirect
+from flask import Blueprint, render_template, request, url_for, redirect, flash
 from flask_login import login_required, current_user
 from .models import *
 import json
@@ -37,28 +37,38 @@ def home():
 #         db.session.commit()
     
         
-# @views.route("/session/<room_id>")
-# @login_required
-# def view_session(room_id):
-#     room=Room.query.filter_by(id=room_id).first()
-#     if not room:
-#         return "ROOM DOES NOT EXIST"
-#     return render_template('code_editor.html')
+@views.route("/session/<room_id>")
+@login_required
+def view_session(room_id):
+    room=Room.query.filter_by(id=room_id).first()
+    if not room:
+        return "ROOM DOES NOT EXIST"
+    return render_template('ide.html',user=current_user)
+    # return render_template('code_editor.html')
 
 @views.route("/projects",methods=['GET','POST'])
 @login_required
 def view_invitations():
     if request.method=='POST': #newroom
         room_name=request.form.get('room_name')
-        new_room=Room(room_name=room_name,owner_id=current_user.id)
-        db.session.add(new_room)
-        db.session.commit() 
+        RoomByName = Room.query.filter_by(room_name=room_name).first()
+        if RoomByName:
+            flash('Room Name already exists.', category='error')
+        else:
+            new_room=Room(room_name=room_name,owner_id=current_user.id)
+            db.session.add(new_room)
+            db.session.commit() 
     
     invited_rooms=InvitedUser.query.filter_by(email=current_user.email).all()
     rooms_dict={}
+
+    created_rooms=Room.query.filter_by(owner_id=current_user.id).all()
+    print(created_rooms)
+    
+
     for rooms in invited_rooms:
         owner=User.query.filter_by(id=rooms.owner_id).first()
         rooms_dict[rooms.room_name]=[rooms.room_language,owner.first_name]
     #Project name #Room Langugae #Owner
-    return render_template('projects.html',user=current_user,rooms_dict=rooms_dict) 
+    return render_template('projects.html',user=current_user,rooms_dict=rooms_dict,created_rooms=created_rooms) 
 
