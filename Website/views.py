29b65aa5d2ac1b03295a8357f704eb226c0cd061
GordonChen19 <chengordon8@gmail.com>
2023-05-20 15:56:32 +0800

    code backend templates

--- Website/views.py
+++ Website/views.py
@@ -3,6 +3,7 @@ from flask_login import login_required, current_user
 from .models import *
 import json
 import sqlite3 as sql
+import runcode
 
 
 conn=sql.connect('database.db')
@@ -36,13 +37,7 @@ def home():
 #         db.session.add(newInvite)
 #         db.session.commit()
     
-        
-@views.route("/session/<room_id>")
-@login_required
-def view_session(room_id):
-    room=Room.query.filter_by(id=room_id).first()
-    
-    return render_template('code_editor.html',user=current_user)
+
 
 @views.route("/projects",methods=['GET','POST'])
 @login_required
@@ -108,3 +103,71 @@ if __name__ == "__main__":
 default_rows = "15"
 default_cols = "60"
 
+@views.route("/session/<room_id>/python",methods=['POST','GET'])
+@login_required
+def enter_room(room_id): 
+    if(request.method=='POST'):
+        code = request.form['code']
+        run = runcode.RunPyCode(code)
+        rescompil, resrun = run.run_py_code()
+        if not resrun:
+            resrun = 'No result!'
+    else:
+        code = default_python_code
+        resrun = 'No result!'
+        rescompil = 'No Compilation for Python'
+        
+    return render_template('code_editor.html',
+                           user=current_user,
+                           code=code,
+                           target="runpy",
+                           resrun=resrun,
+                           rescomp=rescompil,
+                           rows=default_rows,
+                           cols=default_cols)
+
+@views.route("/session/<room_id>/C",methods=['POST','GET'])
+@login_required
+def enter_room(room_id): 
+    if(request.method=='POST'):
+        code = request.form['code']
+        run = runcode.RunCCode(code)
+        rescompil, resrun = run.run_c_code()
+        if not resrun:
+            resrun = 'No result!'
+    else:
+        code = default_c_code
+        resrun = 'No result!'
+        rescompil = ''
+        
+    return render_template('code_editor.html',
+                           user=current_user,
+                           code=code,
+                           target="runc",
+                           resrun=resrun,
+                           rescomp=rescompil,
+                           rows=default_rows,
+                           cols=default_cols)
+
+@views.route("/session/<room_id>/Cpp",methods=['POST','GET'])
+@login_required
+def enter_room(room_id): 
+    if(request.method=='POST'):
+        code = request.form['code']
+        run = runcode.RunCppCode(code)
+        rescompil, resrun = run.run_cpp_code()
+        if not resrun:
+            resrun = 'No result!'
+    else:
+        code = default_cpp_code
+        resrun = 'No result!'
+        rescompil = ''
+        
+    return render_template('code_editor.html',
+                           user=current_user,
+                           code=code,
+                           target="runcpp",
+                           resrun=resrun,
+                           rescomp=rescompil,
+                           rows=default_rows,
+                           cols=default_cols)
