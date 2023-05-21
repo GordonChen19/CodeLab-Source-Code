29e4daf0b70d4a277cab2d6c8141cc325c5681ca
GordonChen19 <chengordon8@gmail.com>
2023-05-22 01:47:57 +0800

    logout routing

--- Website/auth.py
+++ Website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template,request,flash,redirect, url_for
 from werkzeug.security import generate_password_hash,check_password_hash
 from .models import User
 from . import db 
+from .views import views
 from flask_login import login_user,login_required,logout_user,current_user
 
 auth=Blueprint('auth',__name__)
@@ -30,7 +31,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return redirect(url_for('auth.login'))
+    return redirect(url_for('views.home'))
 
 
 @auth.route('/sign-up', methods=['GET', 'POST'])
