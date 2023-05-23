1f864c4457f807a89afebc95f79e30c3393dd731
ivanyjq <IYUEN001@e.ntu.edu.sg>
2023-05-23 22:56:16 +0800

    final

--- Website/auth.py
+++ Website/auth.py
@@ -10,7 +10,9 @@ auth=Blueprint('auth',__name__)
 @auth.route('/login',methods=['GET','POST'])
 def login():
     if request.method == 'POST':
+        error = None
         email=request.form.get('email')
+        print(email)
         password=request.form.get('password')
         user=User.query.filter_by(email=email).first()
         
@@ -20,11 +22,11 @@ def login():
                 login_user(user,remember=True)
                 return redirect(url_for('views.home'))
             else:
-                # flash('Incorrect password, try again.', category='error')
-                pass
+                error = 'Incorrect password, try again.'
+                return render_template("login.html", error=error)
         else:
-            # flash('Email does not exist.', category='error')
-            pass
+            error = 'Email does not exist.'
+            return render_template("login.html", error=error)
         # elif 'sign up button' in request.form:
         #     print("executed")
         #     email = request.form.get('email')
@@ -73,6 +75,7 @@ def logout():
 @auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     if request.method == 'POST':
+        error = None
         email = request.form.get('email')
         first_name = request.form.get('firstName')
         password1 = request.form.get('password1')
@@ -80,20 +83,20 @@ def sign_up():
 
         userByEmail = User.query.filter_by(email=email).first()
         if userByEmail:
-            # flash('Email already exists.', category='error')
-            print("error1")
+            error = "Email already exists."
+            return render_template("login.html", error=error)
         elif len(email) < 4:
-            # flash('Email must be greater than 3 characters.', category='error')
-            print("error3")
+            error = 'Email must be greater than 3 characters.'
+            return render_template("login.html", error=error)
         elif len(first_name) < 2:
-            # flash('First name must be greater than 1 character.', category='error')
-            print("error4")
+            error = 'First name must be greater than 1 character.'
+            return render_template("login.html", error=error)
         elif password1 != password2:
-            # flash('Passwords don\'t match.', category='error')
-            print("error5")
+            error = 'Passwords don\'t match.'
+            return render_template("login.html", error=error)
         elif len(password1) < 7:
-            # flash('Password must be at least 7 characters.', category='error')
-            print("error6")
+            error = 'Password must be at least 7 characters.'
+            return render_template("login.html", error=error)
         else:
             new_user = User(email=email, first_name=first_name, 
                             password=generate_password_hash(password1, method='sha256'))
