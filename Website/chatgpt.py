76c584e97d502d85a1f26938a3646f586d9ba985
Strirex <gbdfaqw@gmail.com>
2023-05-25 03:07:29 +0800

    tokens 1000

--- Website/chatgpt.py
+++ Website/chatgpt.py
@@ -21,7 +21,7 @@ def chatgpt(query):
             {"role": "user", "content": query}
         )
         chat = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages
+            model="gpt-3.5-turbo", messages=messages, max_tokens=1000
         )
     reply = chat.choices[0].message.content
     return reply
