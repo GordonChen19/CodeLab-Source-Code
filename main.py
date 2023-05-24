dd7ee1c6e46b2333f49f60e813dfc2934009a661
GordonChen19 <chengordon8@gmail.com>
2023-05-24 19:56:46 +0800

    removed port 8000

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
 from Website import create_app
-from flask_socketio import SocketIO
 app=create_app()
 
-# parser=argparse.ArgumentParser()
-# parser.add_argument("host",nargs="?")
-# parser.add_argument("port",nargs="?")
-
-# parser.add_argument("-p","--portnum",default="5000")
-# args = parser.parse_args()
-# if not args.host:
-#     args.host = "127.0.0.1"
-# args.port = args.port or args.portnum
-# print("Serving Sockets @ ws://%s:%s"%(args.host,args.port))
-
-
-socketio=SocketIO(app)
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
-    # app.run(debug=True)
+    app.run(debug=True)
     
     
 
