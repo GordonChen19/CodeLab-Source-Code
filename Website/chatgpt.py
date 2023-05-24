68a45a31c60844658f137fe3c4acbe828f72da93
Strirex <gbdfaqw@gmail.com>
2023-05-25 03:50:39 +0800

    why server timeout

--- Website/chatgpt.py
+++ Website/chatgpt.py
@@ -21,7 +21,7 @@ def chatgpt(query):
             {"role": "user", "content": query}
         )
         chat = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=messages, max_tokens=50
+            model="gpt-3.5-turbo", messages=messages, max_tokens=45
         )
     reply = chat.choices[0].message.content
     return reply
